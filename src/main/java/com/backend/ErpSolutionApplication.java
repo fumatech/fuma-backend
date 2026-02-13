7ab85003c855386022ac6f0589c0c45ddb81ce49
Sanket2321 <sanketsonawane5170@gmail.com>
2026-02-13 15:32:41 +0530

    updated code
    updated code

--- src/main/java/com/backend/ErpSolutionApplication.java
+++ src/main/java/com/backend/ErpSolutionApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.backend")
 public class ErpSolutionApplication {
-
 	public static void main(String[] args) {
 
 		SpringApplication.run(ErpSolutionApplication.class, args);
 	}
-
 }
